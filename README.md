0ce63fbc94cbc90340c1705c399506de4af1c5e8
rawlingsj <rawlingsj80@gmail.com>
2016-01-16 11:04:15 +0000

    initial checkin

---
+++ README.md
@@ -0,0 +1,5 @@
+# golang example
+
+This is a very simple golang project that can be imported using the fabric8 console and a CD pipeline applied to build, release, deploy, approve and promote for example.
+
+Once deployed you can access a web page using the openshift route.
---
+++ hello.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<html><head><title>Greetings to the world</title></head><body><h1>Hello from the fabric8 Golang example</h1></body></html>\n")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
+}
